29091ba9ea776267f40165aa0b71bc6bd1e1f4f8
brettjrea <brettjrea@users.noreply.github.com>
2023-03-24 19:35:57 +0000

    Auto commit updated readme.md

--- install-tools.sh
+++ install-tools.sh
@@ -11,3 +11,13 @@ sudo apt install curl git wget -y
 
 # Install Common Compilers
 sudo apt install g++ gcc make -y
+
+# Install Common JSON Processor
+sudo apt install jq -y
+
+# Install the libsecret library for secure password storage
+sudo apt-get install libsecret-1-0 -y
+
+# The libsecret library files are typically installed in the /usr/lib/ directory
+# and header files are located in the /usr/include/libsecret-1/ directory.
+
