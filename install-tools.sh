32a37660fc4bf15de14ddf9abef1252f8aa9453b
brettjrea <brettjrea@users.noreply.github.com>
2023-03-24 20:19:45 +0000

    Auto commit updated readme.md

--- install-tools.sh
+++ install-tools.sh
@@ -19,4 +19,4 @@ sudo apt install jq -y
 sudo apt-get install libsecret-1-0 -y
 
 # The libsecret library files are typically installed in the /usr/lib/ directory
-# and header files are located in the /usr/include/libsecret-1/ directory.
+# and header files are located in the /usr/include/libsecret-1/ directory
